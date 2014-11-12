22e86ca0b627146f5ba22c39ff0f0e075529317d
Sebastian McKenzie <sebmck@gmail.com>
2014-11-13 01:14:52 +1100

    improve website - switch to bootstrap - split up sections

--- Makefile
+++ Makefile
@@ -3,5 +3,6 @@ LESS_COMMAND = node_modules/.bin/lessc
 .PHONY: build
 
 build:
+	rm -rf *.html
 	node scripts/build-template.js
 	$(LESS_COMMAND) src/styles.less styles.css
--- scripts/build-template.js
+++ scripts/build-template.js
@@ -1,35 +1,40 @@
-var fs = require('fs');
-var path = require('path');
-var Handlebars = require('handlebars');
-var marked = require('marked');
-var highlight = require('highlight.js');
-var _ = require('lodash');
+var Handlebars = require("handlebars");
+var highlight  = require("highlight.js");
+var marked     = require("marked");
+var path       = require("path");
+var fs         = require("fs");
+var _s         = require("underscore.string");
+var _          = require("lodash");
 
-var renderer = new marked.Renderer();
+var headings = {};
 
-renderer.link = function(href, title, text) {
-  href = href.toLowerCase().replace('.md', '.html');
+var renderer = new marked.Renderer;
+
+renderer.link = function (href, title, text) {
+  href = href.toLowerCase().replace(".md", ".html");
   return marked.Renderer.prototype.link.apply(this, arguments);
 };
 
-renderer.heading = function(text, level, raw) {
-  raw = raw.toLowerCase()
-    .replace(/[^\w]+/g, '-')
-    .replace(/-$/, '');
+renderer.heading = function (text, level, raw) {
+  raw = _s.dasherize(_s.stripTags(raw.toLowerCase()));
+  var i = headings[raw] || 0;
+  headings[raw] = i + 1;
+  if (i > 0) raw += "-" + i;
+
   text = '<a class="anchor" href="#' + raw + '" aria-hidden="true"><span class="octicon octicon-link"></span></a>' + text;
   return marked.Renderer.prototype.heading.apply(this, arguments);
 };
 
 marked.setOptions({
   renderer: renderer,
-  highlight: function(code, lang) {
+  highlight: function (code, lang) {
     if (!lang) {
       return highlight.highlightAuto(code).value;
     }
 
-    if (lang === 'html') lang = 'erb';
+    if (lang === "html") lang = "erb";
 
-    if (highlight.listLanguages().indexOf(lang) > -1) {
+    if (highlight.listLanguages().indexOf(lang) >= 0) {
       return highlight.highlight(lang, code).value;
     } else {
       return code;
@@ -38,17 +43,25 @@ marked.setOptions({
 });
 
 var template = Handlebars.compile(
-  fs.readFileSync(path.resolve(__dirname, '../src/template.hbs')).toString()
+  fs.readFileSync(path.resolve(__dirname, "../src/template.hbs"), "utf8")
 );
 
-_.each({
-  '6to5/README.md'   : '../index.html',
-  '6to5/LICENSE'     : '../license.html',
-  '6to5/FEATURES.md' : '../features.html',
-  '6to5/MODULES.md'  : '../modules.html'
-}, function(output, input) {
-  var markdown = fs.readFileSync(require.resolve(input)).toString();
-  var content = marked(markdown);
-  var html = template({ content: content });
-  fs.writeFileSync(path.resolve(__dirname, output), html);
+var pkgLoc = require.resolve("6to5/package.json");
+var to5Loc = path.normalize(pkgLoc + "/..");
+
+var pkg     = require(pkgLoc);
+var version = pkg.version;
+
+var pagesLoc = to5Loc + "/doc";
+
+_.each(fs.readdirSync(pagesLoc), function (filename) {
+  var input  = path.join(pagesLoc, filename);
+  var name   = path.basename(filename, ".md");
+  var output = __dirname + "/../" + name + ".html";
+
+  var markdown = fs.readFileSync(input, "utf8");
+  var content  = marked(markdown);
+  var html     = template({ content: content, version: version });
+
+  fs.writeFileSync(output, html);
 });
