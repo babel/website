8c6182467f25c592ade9363f64b9d0810744e6ae
Sebastian McKenzie <sebmck@gmail.com>
2015-01-02 19:25:31 +1100

    2.4.4

--- bin/build-scripts.js
+++ bin/build-scripts.js
@@ -9,10 +9,13 @@ var resolve = function (loc) {
 var to5BrowserLoc = resolve("6to5/browser.js") || resolve("6to5/dist/6to5.js");
 if (!to5BrowserLoc) throw new Error("please run `make build` in 6to5 root");
 
+var to5PolyfillLoc = resolve("6to5/browser-polyfill.js") || resolve("6to5/dist/polyfill.js");
+
 var locs = [
   resolve("jquery/dist/jquery.js"),
   resolve("bootstrap/dist/js/bootstrap.js"),
-  to5BrowserLoc
+  to5BrowserLoc,
+  to5PolyfillLoc
 ];
 
 var fs = require("fs");
