4500417da4b0299d80c3ddb075bc2464e33dbaca
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2015-03-21 22:04:52 +0100

    Move script files to end of body. Closes #19
    - remove render-blocking scripts from page haeder
    - move embedded scripts from markup to separate JavaScript file(s)
    - move all social media related code into single JavaScript file
    - prepare site-wide solution based on front metter declared variables
    - specify layout related external and web site scripts in templates
    - specify page specific external and web site scripts in templates
    All scripts used on the website are now declared using list in front
    metter sections. This allows to build up the correct order of script inclusion
    in the given page and allows the future optimization if script minifying
    is to be introduced.

---
+++ scripts/social.js
@@ -0,0 +1,26 @@
+// Facebook
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+// Twitter
+window.twttr = (function(d, s, id) {
+  var t, js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src="https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+  return window.twttr || (t = { _e: [], ready: function(f) { t._e.push(f) } })
+}(document,"script","twitter-wjs"));
+// Disqus
+/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+var disqus_shortname = '6to5'; // required: replace example with your forum shortname
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
