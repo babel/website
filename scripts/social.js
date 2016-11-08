61056dd53736a34b244d8f77485a5d7b785a76db
Samuel Reed <samuel.trace.reed@gmail.com>
2016-11-08 15:38:42 -0600

    Remove outdated Disqus from posts; add Discourse

--- scripts/social.js
+++ scripts/social.js
@@ -17,13 +17,4 @@ window.twttr = (function(d, s, id) {
   return window.twttr || (t = { _e: [], ready: function(f) { t._e.push(f) } })
 }(document,"script","twitter-wjs"));
 
-// Disqus
-/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-var disqus_shortname = '6to5'; // required: replace example with your forum shortname
 
-/* * * DON'T EDIT BELOW THIS LINE * * */
-(function() {
-  var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-  dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
