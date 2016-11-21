1bd6f2d0bed174527cf961232b5d438332d52b76
Sven SAULEAU <sven.sauleau@xtuc.fr>
2016-11-21 08:33:57 +0100

    refactor(offline): use sw-toolbox

--- service-worker.js
+++ service-worker.js
@@ -1,98 +1,19 @@
 ---
 ---
 
-var version = 'v1';
+importScripts('/scripts/sw-toolbox.js');
 
-self.addEventListener("install", function(event) {
-  console.log('WORKER: install event in progress.');
-  event.waitUntil(
+var swOptions = {
+  debug: true
+}
 
-    caches
-      .open(version)
-      .then(function(cache) {
+var preCachedRessources = [
+  '{{ "/css/main.css" | prepend: site.baseurl }}?t={{ site.time | date_to_xmlschema }}"',
+  {% for page in site.pages %}
+  '{{ page.url }}',
+  {% endfor %}
+];
 
-        return cache.addAll([
-          '/',
-          '{{ "/css/main.css" | prepend: site.baseurl }}?t={{ site.time | date_to_xmlschema }}"',
-          {% for page in site.pages %}
-          '{{ page.url }}',
-          {% endfor %}
+toolbox.precache(preCachedRessources);
 
-        ]);
-      })
-      .then(function() {
-        console.log('WORKER: install completed');
-      })
-  );
-});
-
-self.addEventListener("fetch", function(event) {
-  console.log('WORKER: fetch event in progress.');
-
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then(function(cached) {
-
-        var networked = fetch(event.request)
-          .then(fetchedFromNetwork, unableToResolve)
-          .catch(unableToResolve);
-
-        console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
-        return cached || networked;
-
-        function fetchedFromNetwork(response) {
-          var cacheCopy = response.clone();
-
-          console.log('WORKER: fetch response from network.', event.request.url);
-
-          caches
-            .open(version + 'pages')
-            .then(function add(cache) {
-              cache.put(event.request, cacheCopy);
-            })
-            .then(function() {
-              console.log('WORKER: fetch response stored in cache.', event.request.url);
-            });
-
-          return response;
-        }
-
-        function unableToResolve () {
-          console.log('WORKER: fetch request failed in both cache and network.');
-
-          return new Response('<h1>Service Unavailable</h1>', {
-            status: 503,
-            statusText: 'Service Unavailable',
-            headers: new Headers({
-              'Content-Type': 'text/html'
-            })
-          });
-        }
-      })
-  );
-});
-
-self.addEventListener("activate", function(event) {
-  console.log('WORKER: activate event in progress.');
-
-  event.waitUntil(
-    caches
-      .keys()
-      .then(function (keys) {
-
-        return Promise.all(
-          keys
-            .filter(function (key) {
-              return !key.startsWith(version);
-            })
-            .map(function (key) {
-              return caches.delete(key);
-            })
-        );
-      })
-      .then(function() {
-        console.log('WORKER: activate completed.');
-      })
-  );
-});
+toolbox.router.get('/(.*)', toolbox.cacheFirst, swOptions);
