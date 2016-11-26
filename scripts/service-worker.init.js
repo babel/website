1e7537002c191dfe47f4d3d65e57cbcb3b98d201
Sven SAULEAU <sven.sauleau@xtuc.fr>
2016-11-26 13:47:38 +0100

    feat(sw): improved offline indicator

---
+++ scripts/service-worker.init.js
@@ -0,0 +1,21 @@
+if ('serviceWorker' in navigator) {
+
+  navigator.serviceWorker.register('/service-worker.js').then(function() {
+    var offlineIndicator = $("#offline-indicator");
+
+    window.addEventListener('online',  function() {
+      offlineIndicator.addClass("hidden");
+    });
+
+    window.addEventListener('offline',  function() {
+      offlineIndicator.removeClass("hidden");
+    });
+
+    if (!navigator.onLine) {
+      offlineIndicator.removeClass("hidden");
+    }
+
+  }, function() {
+    console.log('CLIENT: service worker registration failure.');
+  });
+}
