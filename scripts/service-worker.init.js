ac3f4dcf96946a32b4015d0490db884ec2bbe9ae
Sven SAULEAU <sven.sauleau@xtuc.fr>
2016-11-29 14:37:07 +0100

    lint: use single quotes

--- scripts/service-worker.init.js
+++ scripts/service-worker.init.js
@@ -1,18 +1,18 @@
 if ('serviceWorker' in navigator) {
 
   navigator.serviceWorker.register('/service-worker.js').then(function() {
-    var offlineIndicator = $("#offline-indicator");
+    var offlineIndicator = $('#offline-indicator');
 
     window.addEventListener('online',  function() {
-      offlineIndicator.addClass("hidden");
+      offlineIndicator.addClass('hidden');
     });
 
     window.addEventListener('offline',  function() {
-      offlineIndicator.removeClass("hidden");
+      offlineIndicator.removeClass('hidden');
     });
 
     if (!navigator.onLine) {
-      offlineIndicator.removeClass("hidden");
+      offlineIndicator.removeClass('hidden');
     }
 
   }, function() {
--- service-worker.js
+++ service-worker.js
@@ -3,16 +3,16 @@ importScripts('/scripts/sw-toolbox.js');
 var VERSION = '{{ site.time }}';
 
 var contentCacheOptions = {
-  name: "Babel-Cache-" + VERSION,
+  name: 'Babel-Cache-' + VERSION,
   maxEntries: 150,
   maxAgeSeconds: 604800
 }
 
 var cdnCacheOptions = {
-  name: "cdn"
+  name: 'cdn'
 }
 
-toolbox.router.get('/*', toolbox.cacheFirst, { origin: "cdnjs.cloudflare.com", cache: cdnCacheOptions });
-toolbox.router.get('/*', toolbox.cacheFirst, { origin: "cdn.jsdelivr.net", cache: cdnCacheOptions });
+toolbox.router.get('/*', toolbox.cacheFirst, { origin: 'cdnjs.cloudflare.com', cache: cdnCacheOptions });
+toolbox.router.get('/*', toolbox.cacheFirst, { origin: 'cdn.jsdelivr.net', cache: cdnCacheOptions });
 
 toolbox.router.get('/*', toolbox.fastest, { cache: contentCacheOptions });
