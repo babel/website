aa6f6dc3df9cec66e7bc0fdab5c0b0466fc5109f
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-08-07 15:51:46 -0700

    Removed unnecessary App component and repl subdir

--- js/repl/src/App.js
+++
@@ -1,3 +0,0 @@
-import Repl from './repl/Repl.js';
-
-export default Repl;
--- js/repl/src/index.js
+++ js/repl/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Repl from './Repl';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Repl />, document.getElementById('root'));
 registerServiceWorker();
