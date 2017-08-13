e450ce8ee807026f4483b8af9eab1f1b7a224502
Brian Vaughn <brian.david.vaughn@gmail.com>
2017-08-12 18:59:55 -0700

    Expounded on Prettier AST issue TODO

--- js/repl/src/compile.js
+++ js/repl/src/compile.js
@@ -26,14 +26,25 @@ export default function compile(code: string, config: CompileConfig): Return {
   let evalError = null;
 
   try {
-    compiled = window.Babel.transform(code, {
+    const transformed = window.Babel.transform(code, {
       presets: config.presets,
       plugins: ['transform-regenerator']
-    }).code;
+    });
+
+    compiled = transformed.code;
 
     if (config.prettify && window.prettier !== undefined) {
       // TODO Don't re-parse; just pass Prettier the AST we already have.
+      // This will have to wait until we've updated to Babel 7 since Prettier uses it.
+      // Prettier doesn't handle ASTs from Babel 6.
+      // if (
+      //   window.prettier.__debug !== undefined &&
+      //   typeof window.prettier.__debug.formatAST === 'function'
+      // ) {
+      //   compiled = window.prettier.__debug.formatAST(transformed.ast, DEFAULT_PRETTIER_CONFIG);
+      // } else {
       compiled = window.prettier.format(compiled, DEFAULT_PRETTIER_CONFIG);
+      // }
     }
 
     if (config.evaluate) {
