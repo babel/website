c570741d7f04cfa2578ac4f3a63119c81444d334
Mateusz Burzyński <mateuszburzynski@gmail.com>
2017-08-18 00:00:13 +0200

    Prepared a precommit hook, flow + prettier

--- js/repl/index.js
+++ js/repl/index.js
@@ -1,11 +1,12 @@
 // @flow
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Repl from './Repl';
+import React from "react";
+import ReactDOM from "react-dom";
+import Repl from "./Repl";
 
-ReactDOM.render(<Repl />, document.getElementById('root'));
+ReactDOM.render(<Repl />, document.getElementById("root"));
 
 if (module.hot) {
+  // $FlowFixMe
   module.hot.accept();
 }
