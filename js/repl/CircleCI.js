13d095a3a75038a1c98c6c27c96c58b41843e8d2
Daniel Lo Nigro <daniel@dan.cx>
2017-08-29 22:13:52 -0700

    Use fetch

--- js/repl/CircleCI.js
+++ js/repl/CircleCI.js
@@ -1,32 +1,22 @@
 // @flow
 
-function sendRequest(repo: ?string, uri: string): Promise<Object> {
-  return new Promise((resolve, reject) => {
-    const urlRepo = repo && repo.length ? repo : "babel/babel";
-    const fullURL = `https://circleci.com/api/v1.1/project/github/${urlRepo}/${uri}`;
-    const xhr = new XMLHttpRequest();
-    xhr.open("get", fullURL, true);
-    xhr.onload = function() {
-      let response;
-      try {
-        response = JSON.parse(xhr.responseText);
-      } catch (ex) {
-        reject(new Error("Invalid JSON"));
-        return;
-      }
-      // CircleCI sometimes returns errors as 200 (OK) responses with a "message"
-      // field...
-      if (response.message) {
-        reject(new Error(response.message));
-        return;
-      }
-      resolve(response);
-    };
-    xhr.onerror = function() {
-      reject(new Error("Error sending request to CircleCI :("));
-    };
-    xhr.send();
-  });
+import fetch from "unfetch";
+
+async function sendRequest(repo: ?string, uri: string): Promise<Object> {
+  const urlRepo = repo && repo.length ? repo : "babel/babel";
+  const fullURL = `https://circleci.com/api/v1.1/project/github/${urlRepo}/${uri}`;
+  let response;
+  try {
+    response = await fetch(fullURL).then(res => res.json());
+  } catch (ex) {
+    throw new Error(`Error sending request to CircleCI: ${ex.message}`);
+  }
+  // CircleCI sometimes returns errors as 200 (OK) responses with a "message"
+  // field...
+  if (response.message) {
+    throw new Error(response.message);
+  }
+  return response;
 }
 
 export async function loadBuildArtifacts(
