46a760081607572514fd7366b798dc9c860cbf58
QC-L <liqichang_4869@163.com>
2017-10-31 16:21:13 +0800

    fix CI bugs: 修改配置错误 by QC-L

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,9 +1,10 @@
+make bootstrap
 make build
 
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO}
-git config --global user.name "Travis CI"
-git config --global user.email "ci@travis-ci.org"
+git config --global user.name "QC-L"
+git config --global user.email "liqichang_4869@163.com"
 git remote set-url origin "${SSH_REPO}"
 
 openssl aes-256-cbc -K $encrypted_9d42543f6ef0_key -iv $encrypted_9d42543f6ef0_iv -in shell/deploy_key.enc -out shell/deploy_key -d
