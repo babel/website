50da4cd5abbb2b3501e74ae0a79b293f951f8eec
QC-L <liqichang_4869@163.com>
2017-10-31 20:54:58 +0800

    fix bugs

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -6,6 +6,8 @@ git config --global user.name "Travis CI"
 git config --global user.email "ci@travis-ci.org"
 git remote set-url origin "${SSH_REPO}"
 
+
+openssl aes-256-cbc -K $encrypted_6f04629492b4_key -iv $encrypted_6f04629492b4_iv in shell/deploy_key.enc -out shell/deploy_key -d
 chmod 600 shell/deploy_key
 eval `ssh-agent -s`
 ssh-add shell/deploy_key
