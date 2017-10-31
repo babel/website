5b4b42a53dc629fca730513e200322985ac4494e
QC-L <liqichang_4869@163.com>
2017-10-31 21:16:33 +0800

    fix bugs

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -1,10 +1,8 @@
 npm run build
 
-REPO=`git config remote.origin.url`
-SSH_REPO=${REPO}
 git config --global user.name "Travis CI"
 git config --global user.email "ci@travis-ci.org"
-git remote set-url origin "${SSH_REPO}"
+git remote set-url origin git@github.com:docschina/babeljs.io.git
 
 
 openssl aes-256-cbc -K $encrypted_6f04629492b4_key -iv $encrypted_6f04629492b4_iv -in shell/deploy_key.enc -out shell/deploy_key -d
