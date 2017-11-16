a2b654a9588ca52a5cf74c0fc18ca91617203ac6
Henry Zhu <hi@henryzoo.com>
2017-11-16 09:46:20 -0500

    fix loading babel standalone (#1465)

--- js/repl/loadBabel.js
+++ js/repl/loadBabel.js
@@ -83,7 +83,7 @@ export default async function loadBabel(
   }
 
   const babelStandalone =
-    (semver.valid(version) && semver.gte(version, "7.0.0-beta.5")) ||
+    (semver.valid(version) && semver.gte(version, "7.0.0-beta.4")) ||
     version >= 7
       ? "@babel/standalone"
       : "babel-standalone";
