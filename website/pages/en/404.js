1e4feaa3160edfdfa7ba3c9a70e6d91ab3ae6d36
Greg Borrelly <gregborrelly@gmail.com>
2018-01-30 15:10:37 -0500

    Adding styling to ErrorPage Component

--- website/pages/en/404.js
+++ website/pages/en/404.js
@@ -4,17 +4,30 @@ const React = require("react");
 class ErrorPage extends React.Component {
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8 col-md-push-2">
-                        <script type="text/javascript">
-                            var GOOG_FIXURL_LANG = 'en';
-                             var GOOG_FIXURL_SITE = 'https://babeljs.io/'
-                        </script>
-                        <script type="text/javascript" src="https://linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js"></script>
+
+            <div className="error-page">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 error-message">
+                            <div className=" error-message-container container">
+                                <span>404 </span>
+                                <p>Page Not Found.</p>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12 error-body">
+                            <script type="text/javascript">
+                                var GOOG_FIXURL_LANG = 'en';
+             var GOOG_FIXURL_SITE = 'https://babeljs.io/'
+        </script>
+                            <script type="text/javascript" src="https://linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js"></script>
+                        </div>
                     </div>
                 </div>
             </div>
+
         );
     }
 }
