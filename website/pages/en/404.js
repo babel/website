7d311e4bd72a0a2e2e0cbccd7eea152c474b02d5
Greg Borrelly <gregborrelly@gmail.com>
2018-01-30 15:34:18 -0500

    Removing Boostrap styling from  ErrorPage Component

--- website/pages/en/404.js
+++ website/pages/en/404.js
@@ -6,26 +6,23 @@ class ErrorPage extends React.Component {
         return (
 
             <div className="error-page">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12 error-message">
-                            <div className=" error-message-container container">
-                                <span>404 </span>
-                                <p>Page Not Found.</p>
-                            </div>
-
-                        </div>
+                <div className="error-message">
+                    <div className=" error-message-container container">
+                        <span>404 </span>
+                        <p>Page Not Found.</p>
                     </div>
-                    <div className="row">
-                        <div className="col-md-12 error-body">
-                            <script type="text/javascript">
-                                var GOOG_FIXURL_LANG = 'en';
+
+                </div>
+
+
+                <div className="error-body">
+                    <script type="text/javascript">
+                        var GOOG_FIXURL_LANG = 'en';
              var GOOG_FIXURL_SITE = 'https://babeljs.io/'
         </script>
-                            <script type="text/javascript" src="https://linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js"></script>
-                        </div>
-                    </div>
+                    <script type="text/javascript" src="https://linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js"></script>
                 </div>
+
             </div>
 
         );
