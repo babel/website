996632815204c27801a48710281335d63818f61d
Greg Borrelly <gregborrelly@gmail.com>
2018-01-29 20:18:20 -0500

    adding 404 page

---
+++ website/pages/en/404.js
@@ -0,0 +1,23 @@
+const React = require("react");
+
+
+class ErrorPage extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 col-md-push-2">
+                        <script type="text/javascript">
+                            var GOOG_FIXURL_LANG = 'en';
+                             var GOOG_FIXURL_SITE = 'https://babeljs.io/'
+                        </script>
+                        <script type="text/javascript" src="https://linkhelp.clients.google.com/tbproxy/lh/wm/fixurl.js"></script>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+module.exports = ErrorPage;
