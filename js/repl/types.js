773a6bd3fe3562eccaf7c3b4262a4ef88dc33884
Rahul Dhawani <dhawani009@gmail.com>
2018-05-03 18:17:50 +0530

    Update types.js

--- js/repl/types.js
+++ js/repl/types.js
@@ -25,6 +25,7 @@ export type LoadScriptCallback = (success: boolean) => void;
 
 export type PluginConfig = {
   baseUrl?: string,
+  help?: string,
   isPreLoaded?: boolean,
   label: string,
   package: string,
