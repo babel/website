224a96f58c5ee49215ff476cb95664909dd05fd1
gyujincho <gyujincho@ridi.com>
2018-08-14 21:32:39 +0900

    Fix es5-shim to be loaded from cdn

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -124,6 +124,7 @@ const siteConfig = {
     theme: "tomorrow",
   },
   scripts: [
+    "https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.7/es5-shim.min.js",
     "https://unpkg.com/clipboard@2.0.0/dist/clipboard.min.js",
     "/js/build/code-blocks-buttons.js",
   ],
