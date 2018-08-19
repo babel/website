ea2bee044ba618dd4ff1c9401595834a96734e10
QC-L <github@liqichang.com>
2018-08-19 18:21:49 +0800

    fix: footer css pollutes the whole

---
+++ website/static/scripts/hide-footer.js
@@ -0,0 +1,9 @@
+(function() {
+  const pathname = window.document.location.pathname;
+  const footer = document.getElementById("footer");
+  if (pathname.indexOf("/repl") != -1) {
+    footer.setAttribute("class", "nav-footer hide-footer");
+  } else {
+    footer.setAttribute("class", "nav-footer");
+  }
+})();
