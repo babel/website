1674225595383549948948decaac6c389465ca70
Brian Ng <bng412@gmail.com>
2018-08-20 17:51:06 -0500

    ensure dom is ready

--- website/static/scripts/hide-footer.js
+++ website/static/scripts/hide-footer.js
@@ -1,9 +1,11 @@
 (function() {
-  const pathname = window.document.location.pathname;
-  const footer = document.getElementById("footer");
-  if (pathname.indexOf("/repl") != -1) {
-    footer.setAttribute("class", "nav-footer hide-footer");
-  } else {
-    footer.setAttribute("class", "nav-footer");
-  }
+  document.addEventListener("DOMContentLoaded", function() {
+    const pathname = window.document.location.pathname;
+    const footer = document.getElementById("footer");
+    if (pathname.indexOf("/repl") != -1) {
+      footer.setAttribute("class", "nav-footer hide-footer");
+    } else {
+      footer.setAttribute("class", "nav-footer");
+    }
+  });
 })();
