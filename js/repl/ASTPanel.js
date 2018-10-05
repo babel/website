c97537944b6e74842079057f01d937b5c96c6fb1
sujinleeme <sujinlee.me@gmail.com>
2018-10-05 19:33:18 +0900

    Trigger show/hide type keys in AST ouput object

--- js/repl/ASTPanel.js
+++ js/repl/ASTPanel.js
@@ -1,17 +1,26 @@
 // @flow
 
+import { css } from "emotion";
+import { colors } from "./styles";
 import React from "react";
 import ReactJson from "react-json-view";
-import { flatten, unflatten, sortedKeys } from "./ASTUtils";
+import {
+  flatten,
+  unflatten,
+  filterFlatten,
+  deleteFlatten,
+  mergeFlatten,
+} from "./ASTUtils";
 
 type Props = {
+  className?: string,
   src: Object,
 };
 
 type State = {
   src: Object,
   flattenSrc: Object,
-  deleteSrc: Object,
+  flattenType: Object,
   astOption: {
     autofocus: boolean,
     location: boolean,
@@ -26,7 +35,7 @@ export default class ASTPanel extends React.Component<Props, State> {
   state = {
     src: {},
     flattenSrc: {},
-    deleteSrc: {},
+    flattenType: {},
     astOption: {
       autofocus: true,
       location: true,
@@ -41,6 +50,7 @@ export default class ASTPanel extends React.Component<Props, State> {
       return {
         src: unflatten(flattenSrc),
         flattenSrc: flattenSrc,
+        flattenType: filterFlatten(flattenSrc, "type"),
       };
     }
     return null;
@@ -53,26 +63,60 @@ export default class ASTPanel extends React.Component<Props, State> {
         [option]: !prevState.astOption[option],
       },
     }));
+
+    this.onChangeJson(option);
+  }
+
+  onChangeJson(option: string) {
+    const { src, astOption, flattenSrc, flattenType } = this.state;
+    // const OPTION_ORDER = ["autofocus", "location", "empty", "type"];
+
+    function triggerAstOutput(type) {
+      const isShow = astOption[type];
+      let newSrc = {};
+      const types = {
+        autofocus: () => {},
+        empty: () => {},
+        type: () => {
+          if (isShow) {
+            newSrc = deleteFlatten(flattenSrc, flattenType);
+          } else {
+            newSrc = mergeFlatten(flattenSrc, flattenType);
+          }
+          return newSrc;
+        },
+        location: () => {
+          console.log(isShow, type);
+        },
+        default: () => {},
+      };
+      return (types[type] || types["default"])();
+    }
+
+    const result = triggerAstOutput(option);
+    this.setState({ flattenSrc: result, src: unflatten(result) });
   }
 
   render() {
     const { src, astOption } = this.state;
+    const { className = "" } = this.props;
 
     return (
-      <div>
-        {OPTION_ORDER.map(option => {
-          return (
-            <label>
-              <input
-                checked={astOption[option]}
-                // className={styles.inputCheckboxLeft}
-                type="checkbox"
-                onChange={() => this.onOptionSettingCheck(option)}
-              />
-              {option}
-            </label>
-          );
-        })}
+      <div className={`${styles.panel} ${className}`}>
+        <div>
+          {OPTION_ORDER.map(option => {
+            return (
+              <label>
+                <input
+                  checked={astOption[option]}
+                  type="checkbox"
+                  onChange={() => this.onOptionSettingCheck(option)}
+                />
+                {option}
+              </label>
+            );
+          })}
+        </div>
         {src && (
           <ReactJson
             src={src}
@@ -90,3 +134,34 @@ export default class ASTPanel extends React.Component<Props, State> {
     );
   }
 }
+
+const styles = {
+  codeMirror: css({
+    display: "block",
+    height: "100%",
+    width: "100%",
+    overflow: "auto",
+    position: "relative",
+  }),
+  // error: css({
+  //   order: 2,
+  //   backgroundColor: colors.errorBackground,
+  //   borderTop: `1px solid ${colors.errorBorder}`,
+  //   color: colors.errorForeground,
+  //   ...sharedBoxStyles,
+  // }),
+  // info: css({
+  //   order: 1,
+  //   backgroundColor: colors.infoBackground,
+  //   borderTop: `1px solid ${colors.infoBorder}`,
+  //   color: colors.infoForeground,
+  //   ...sharedBoxStyles,
+  // }),
+  panel: css({
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "stretch",
+    overflow: "auto",
+  }),
+};
