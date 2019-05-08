939acccf586acad666049b2360d5321eacb1024c
Daniel Tschinder <daniel@tschinder.de>
2019-05-08 01:19:32 -0700

    Upgrade babel to v7

---
+++ babel.config.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = function(api) {
+  const env = api.env();
+  api.cache.using(() => env);
+
+  const plugins = ["@babel/plugin-proposal-class-properties"];
+
+  if (env === "production") {
+    plugins.push("emotion");
+  } else if (env === "development" || !env) {
+    plugins.push(["emotion", { sourceMap: true }]);
+  }
+
+  return {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          useBuiltIns: "usage",
+          corejs: 3,
+          shippedProposals: true,
+        },
+      ],
+      "@babel/preset-flow",
+      "@babel/preset-react",
+    ],
+    plugins,
+  };
+};
--- js/repl/index.js
+++ js/repl/index.js
@@ -1,6 +1,5 @@
 // @flow
 
-import "@babel/polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 
