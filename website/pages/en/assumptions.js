101e4525b1145a7b58a2078711fe28c8cbbfda81
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-01-14 02:54:44 +0100

    More docs

--- website/pages/en/assumptions.js
+++ website/pages/en/assumptions.js
@@ -33,7 +33,10 @@ const Assumption = ({ name, code, plugins, children }) => (
 class AssumptionsDocs extends React.Component {
   render() {
     return (
-      <div className="mainContainer" style={{ maxWidth: "1024px", margin: "0 auto" }}>
+      <div
+        className="mainContainer"
+        style={{ maxWidth: "1024px", margin: "0 auto" }}
+      >
         <script src="/js/babel.min.js" />
         <script type="module" src="/js/components/mini-repl.js" />
         <script type="module" src="/js/components/assumption-docs.js" />
@@ -59,6 +62,19 @@ class AssumptionsDocs extends React.Component {
           <br />
           This can be useful, for example, to iterate DOM collections in older
           browsers
+        </Assumption>
+        <Assumption name="constantSuper" code={`
+          class Child extends Base {
+            method() {
+              super.method(2);
+            }
+          }
+        `} plugins="transform-classes">
+          The super class of a class can be changed at any time by using{" "}
+          <code>Object.setPrototypeOf</code>, making it impossible for Babel to
+          statically know it. When this option is enabled, Babel assumes that
+          it's never changed and thus it is always the value that was placed in
+          the <code>extends</code> clause in the class declaration.
         </Assumption>
         <Assumption
           name="ignoreFunctionLength"
@@ -143,6 +159,38 @@ class AssumptionsDocs extends React.Component {
           if they are not copied.
         </Assumption>
         <Assumption
+          name="privateFieldsAsProperties"
+          code={`
+            class Foo {
+                #method() {}
+
+                #field = 2;
+            }
+        `}
+          plugins="proposal-class-properties,proposal-private-methods"
+        >
+          Assume that "soft privacy" is enough for private fields, and thus they
+          can be stored as public non-enumerable properties with an unique name
+          (rather than using an external <code>WeakMap</code>). This makes
+          debugging compiled private fields easier.
+        </Assumption>
+        <Assumption
+          name="setClassMethods"
+          code={`
+            class Foo extends Bar {
+                method() {}
+
+                static check() {}
+              }
+          `}
+          plugins="transform-classes"
+        >
+          When declaring classess, assume that methods don't shadow getters on
+          the superclass and that the program doesn't depend on methods being
+          non-enumerable. Thus, it's safe to assign methods rather than using{" "}
+          <code>Object.defineProperty</code>.
+        </Assumption>
+        <Assumption
           name="setComputedProperties"
           code={`
             let obj = {
@@ -155,6 +203,22 @@ class AssumptionsDocs extends React.Component {
           When using computed object properties, assume that the object doesn't
           contain properties that overwrite setter defined in the same object,
           and thus it's safe to assign them rather than defining them using{" "}
+          <code>Object.defineProperty</code>.
+        </Assumption>
+        <Assumption
+          name="setPublicClassFields"
+          code={`
+            class Test {
+              field = 2;
+
+              static staticField = 3;
+            }
+          `}
+          plugins="proposal-class-properties"
+        >
+          When using public class fields, assume that they don't shadow any
+          getter in the current class, in its subclasses or in its superclass.
+          Thus, it's safe to assign them rather than using{" "}
           <code>Object.defineProperty</code>.
         </Assumption>
         <Assumption
@@ -184,6 +248,21 @@ class AssumptionsDocs extends React.Component {
           closed with <code>.return()</code> and with <code>.throw()</code> in
           case of an error. When this option is called Babel assumes that those
           methods are not defined or empty, and it avoids calling them.
+        </Assumption>
+        <Assumption
+          name="superIsCallableConstructor"
+          code={`
+            class Child extends Parent {
+              constructor() {
+                super(42);
+              }
+            }
+          `}
+          plugins="transform-classes"
+        >
+          When extending classes, assume that the super class is callable. This
+          means that it won't be possible to extend native classes or built-ins,
+          but only compiled classes or ES5 <code>function</code> constructors.
         </Assumption>
         <br />
         <br />
