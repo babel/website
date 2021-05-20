9cf5c119e319aedfe98f3c9f114a7a8b82c1e8fa
coderaiser <mnemonic.enemy@gmail.com>
2021-05-21 01:15:50 +0300

    Updated link to latest version blog post: 7.13.0 -> 7.14.0 (#2534)

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -199,9 +199,9 @@ const Hero = ({ language }) => (
 
       <div className="hero__announcement">
         <span>
-          <strong>Babel 7.13 is released!</strong> Please read our{" "}
-          <a href="blog/2021/02/22/7.13.0">blog post</a> for highlights and{" "}
-          <a href="https://github.com/babel/babel/releases/tag/v7.13.0">
+          <strong>Babel 7.14 is released!</strong> Please read our{" "}
+          <a href="blog/2021/04/29/7.14.0">blog post</a> for highlights and{" "}
+          <a href="https://github.com/babel/babel/releases/tag/v7.14.0">
             changelog
           </a>{" "}
           for more details!
