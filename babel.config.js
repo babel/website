37be671f8be2d91236ed7718042d8836b113c342
Huáng Jùnliàng <jlhwung@gmail.com>
2023-03-09 09:00:59 -0500

    Update babel config (#2753)

--- babel.config.js
+++ babel.config.js
@@ -1,34 +1,37 @@
 "use strict";
 
 module.exports = function (api) {
-  const env = api.env();
-  api.cache.using(() => env);
-
-  const plugins = ["@babel/plugin-proposal-class-properties"];
-
-  if (env === "production") {
-    plugins.push("emotion");
-  } else if (env === "development" || !env) {
-    plugins.push(["emotion", { sourceMap: true }]);
-  }
-
+  api.cache(true);
   return {
+    targets: {
+      // This is browserslist defaults
+      browsers: ["> 0.5%", "last 2 versions", "Firefox ESR", "not dead"],
+    },
     presets: [
       [
         "@babel/preset-env",
         {
           useBuiltIns: "entry",
-          corejs: 3,
+          corejs: require("core-js/package.json").version,
           shippedProposals: true,
-          targets: {
-            // This is browserslist defaults
-            browsers: ["> 0.5%", "last 2 versions", "Firefox ESR", "not dead"],
-          },
         },
       ],
       "@babel/preset-react",
-      "@babel/preset-typescript",
+      [
+        "@babel/preset-typescript",
+        {
+          allowDeclareFields: true,
+          onlyRemoveTypeImports: true,
+        },
+      ],
     ],
-    plugins,
+    env: {
+      production: {
+        plugins: ["emotion"],
+      },
+      development: {
+        plugins: [["emotion", { sourceMap: true }]],
+      },
+    },
   };
 };
