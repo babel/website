25816124df43e946845b51b5ddef5a6e121bf55e
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-05-21 16:11:44 +0200

    Remove duplicate users list (#2786)

--- website/src/pages/users.js
+++ website/src/pages/users.js
@@ -58,44 +58,6 @@ const Users = () => {
           </div>
         </div>
       </div>
-
-      <div className="mainContainer">
-        <div class="container paddingBottom">
-          <div class="wrapper">
-            <div className="mainContainer">
-              <div className="showcaseSection">
-                <div className="prose">
-                  <h1>See who is using Babel</h1>
-                  <p>
-                    Logos are submitted by company and project representatives.
-                    These companies may or may not be using Babel on their main
-                    web properties, but they are definitely using it somewhere
-                    in their organizations 🙂
-                  </p>
-                </div>
-                <hr />
-                <div className="logos">{showcase}</div>
-                <hr />
-                <div className="prose">
-                  <p>
-                    Are you using this project? Please submit a logo of 500x200
-                    (2.5x1) run through{" "}
-                    <a href="https://jakearchibald.github.io/svgomg/">SVGO</a>
-                  </p>
-                  <p>And give us a shout on what you love about Babel!</p>
-                  <br />
-                  <a
-                    href="https://github.com/babel/website/edit/main/website/data/users.yml"
-                    className="button button--primary"
-                  >
-                    Submit a Pull Request!
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </Layout>
   );
 };
