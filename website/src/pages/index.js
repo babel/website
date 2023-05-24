6e8d47dafbf951f3561157ee411a727e8709ebbe
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-05-24 16:12:56 +0200

    Fix

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -220,7 +220,7 @@ const Hero = ({ language }) => (
       <div className="hero__announcement">
         <span>
           <strong>Babel 7.22 is released!</strong> Please read our{" "}
-          <Link to="/7.22.0">blog post</Link> for highlights and{" "}
+          <Link to="/blog/2023/05/26/7.22.0">blog post</Link> for highlights and{" "}
           <Link to="https://github.com/babel/babel/releases/tag/v7.22.0">
             changelog
           </Link>{" "}
