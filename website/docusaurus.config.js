0fb736ae8162cc2af8efda2068d9d7ab3bded2ff
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2024-11-29 12:06:23 +0800

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -272,7 +272,7 @@ const siteConfig = {
 
   scripts: [
     {
-      src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js",
+      src: "https://unpkg.com/jquery@1.9.1/jquery.min.js",
       defer: true,
     },
     {
